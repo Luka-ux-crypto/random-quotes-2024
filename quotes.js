25dd8e7392aed731bd64c70cdea40a14d51738cb
Stanislav <alina.malina.ivanova.1989@gmail.com>
2024-11-25 18:56:38 +0200

    Splitting app into the JS module

---
+++ quotes.js
@@ -0,0 +1,34 @@
+const quotes = [
+  {
+    quote: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    quote: 'Life is 10% what happens to us and 90% how we react to it',
+    author: 'Charles R. Swindoll',
+  },
+  {
+    quote:
+      'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: "Believe you can and you're halfway there",
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+  },
+  {
+    quote: "Don't watch the clock; do what it does. Keep going",
+    author: 'Sam Levenson',
+  },
+  {
+    quote:
+      'Keep your face always toward the sunshine—and shadows will fall behind you',
+    author: 'Walt Whitman',
+  },
+];
+
+export default quotes;
