5470cb304511a93ff1217d1a1312031df9c50d65
Stanislav <alina.malina.ivanova.1989@gmail.com>
2024-11-26 13:58:25 +0200

    Show favorite cardsbelow current quote

--- quotes.js
+++ quotes.js
@@ -2,32 +2,39 @@ const quotes = [
   {
     quote: 'The best way to predict the future is to invent it',
     author: 'Alan Kay',
+    isFavorite: false,
   },
   {
     quote: 'Life is 10% what happens to us and 90% how we react to it',
     author: 'Charles R. Swindoll',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: It is the courage to continue that counts',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: "Believe you can and you're halfway there",
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'Act as if what you do makes a difference. It does.',
     author: 'William James',
+    isFavorite: false,
   },
   {
     quote: "Don't watch the clock; do what it does. Keep going",
     author: 'Sam Levenson',
+    isFavorite: false,
   },
   {
     quote:
       'Keep your face always toward the sunshine—and shadows will fall behind you',
     author: 'Walt Whitman',
+    isFavorite: false,
   },
 ];
 
--- script.js
+++ script.js
@@ -4,18 +4,59 @@ import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const quoteAuthorElement = document.getElementById('quote - author');
 const generateBtn = document.getElementById('new-quote');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
-
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? 'Remove from favorite'
+    : 'Add to favorite';
+  toggleFavoriteBtn.style.display = 'inline-block';
   //quoteElement.textContent = randomQuote.quote;
   //quoteAuthorElement.textContent = randomQuote.author;
 }
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? 'Remove from favorite'
+    : 'Add to favorite';
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favorite-card');
+    favoriteCard.innerHTML = `
+  <p>${currentQuote.quote}</p>
+  <p class="author">${currentQuote.author}</p>`;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
+}
+
 generateBtn.addEventListener('click', generateRandomQuote);
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
+generateRandomQuote();
+
+// Либо с помощью if
+/*if (toggleFavoriteBtn.textContent === 'Add to favorite') {
+    toggleFavoriteBtn.textContent = 'Remove from favorite';
+  } else {
+    toggleFavoriteBtn.textContent = 'Add to favorite';
+  }*/
 
 // Add an event listener to the button
 //document.getElementById('new-quote').addEventListener('click', displayRandomQuote);
