4ddd4e5ceeab87ca17319f40bb952cfbb92027a1
Stanislav <alina.malina.ivanova.1989@gmail.com>
2024-11-28 15:45:13 +0200

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,39 @@
+const quotes = [
+  {
+    text: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+    isFavorite: false,
+  },
+  {
+    text: 'Life is 10% what happens to us and 90% how we react to it',
+    author: 'Charles R. Swindoll',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: "Believe you can and you're halfway there",
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+    isFavorite: false,
+  },
+  {
+    text: "Don't watch the clock; do what it does. Keep going",
+    author: 'Sam Levenson',
+    isFavorite: false,
+  },
+  {
+    text: 'Keep your face always toward the sunshine—and shadows will fall behind you',
+    author: 'Walt Whitman',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
